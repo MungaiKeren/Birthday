e4ce0ee35dd22c64d1bd82b7045e553022733681
MungaiKeren <wambukeren@gmail.com>
2021-08-24 00:55:24 +0300

    output input date using the date function

--- birthday.py
+++ birthday.py
@@ -1,7 +1,20 @@
-import datetime
+# import the three main classes from datetime
+from datetime import date, time, datetime
 
 fname = input("Type in your first name: ")
 lname = input("Your last name: ")
 print("Hello "+fname+" "+lname)
 
-birthdate = input("When were you born - YYYY/MM/DD")
+birthyear = input("What year were you born?")
+converted_year = int(birthyear) # int function converts string input to integer
+
+birthmonth = input("Month?")
+c_month = int(birthmonth)
+
+birthdate = input("Day?")
+c_date = int(birthdate)
+
+print("Birthdate: ", date(year=converted_year, month=c_month, day=c_date)) # date function 'combines' the dates
+
+
+
