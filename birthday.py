ccf8bc0b70a1f9a700d0db4d4a9b478d091079dd
MungaiKeren <wambukeren@gmail.com>
2021-08-23 23:46:13 +0300

    started working on understanding dates and time in python

---
+++ birthday.py
@@ -0,0 +1,7 @@
+import datetime
+
+fname = input("Type in your first name: ")
+lname = input("Your last name: ")
+print("Hello "+fname+" "+lname)
+
+birthdate = input("When were you born - YYYY/MM/DD")
