c5a2d67438d8d68b6aa88f2f9d2df84f3d3ca370
MungaiKeren <wambukeren@gmail.com>
2021-09-07 11:37:24 +0300

    validate the input birth year

--- birthday.py
+++ birthday.py
@@ -5,10 +5,16 @@ fname = input("Type in your first name: ")
 lname = input("Your last name: ")
 print("Hello "+fname+" "+lname)
 
-birthyear = int(input("What year were you born?"))
+while True:
+    try:
+        birthyear = input("What year were you born?")
+        if not len(birthyear) == 4:
+            print("Please give us a valid date, eg 1999 ")
+        continue
+    except ValueError:
+        # age successfully parsed
+        break
 
-if len(birthyear) < 4:
-    print("Please enter a valid year")
 
 birthmonth = input("Month?")
 c_month = int(birthmonth)
@@ -16,7 +22,7 @@ c_month = int(birthmonth)
 birthday = input("Day?")
 c_date = int(birthday)
 
-birthdate = date(year=birthyear, month=c_month, day=c_date)
+birthdate = date(year=int_year, month=c_month, day=c_date)
 
 print("Birthdate: ", birthdate) # date function 'combines' the dates
 today = date.today()
